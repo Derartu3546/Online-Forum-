10768940330cb7a52eddfb1aa0385e99ccc8ca87
desu kasaye <desukasaye19@gmail.com>
2025-02-25 14:51:43 +0900

    modification of validation form and back background image

--- signup.php
+++ signup.php
@@ -1,17 +1,33 @@
 <?php
 include 'db.php';
-
+$userError=$passwordError=$emailError="";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $email = $_POST['email'];
 
-    $stmt = $pdo->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
-    if ($stmt->execute([$username, $password, $email])) {
-        echo "Registration successful!";
-    } else {
-        echo "Error: User could not be registered.";
+    if(empty($username)){
+        $userError="User name is required";
     }
+    elseif(empty($password)){
+            $passwordError="password is Required";
+
+    } elseif(empty($email)){
+        $emailError="Email is Required";
+     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Enter valid email format";
+    }
+    }
+    else{
+        $stmt = $pdo->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
+        if ($stmt->execute([$username, $password, $email])) {
+            echo "Registration successful!";
+        } else {
+            echo "Error: User could not be registered.";
+        }
+    }
+   
 }
 ?>
 
@@ -25,8 +41,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <body> <div class="box">
     <form method="POST" action="">
         <input type="text" name="username" placeholder="Username" required>
+        <div class="error"><?php echo $userError?></div>
         <input type="password" name="password" placeholder="Password" required>
+        <div class="error"><?php echo $passwordError?></div>
         <input type="email" name="email" placeholder="Email" required>
+        <div class="error"><?php echo $emailError?></div>
         <button type="submit">signup</button>
     </form>
     </div>
